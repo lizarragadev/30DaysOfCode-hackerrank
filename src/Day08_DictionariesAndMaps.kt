47a90a2478cc9798c9b991b239291150ccbdff0f
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2022-02-25 20:29:42 -0400

    Solved Day 8 in Kotlin
    No function hasNext in while

---
+++ src/Day08_DictionariesAndMaps.kt
@@ -0,0 +1,36 @@
+import java.io.PrintWriter
+import java.util.*
+import kotlin.math.pow
+
+val map = mutableMapOf<String, String>()
+
+fun main(args: Array<String>) {
+    _writer.day08()
+    _writer.flush()
+}
+
+fun PrintWriter.day08() {
+    val n = readInt()
+    if(n in 1..10.toDouble().pow(5.toDouble()).toInt()) {
+        fillMap08(n)
+        val sc = Scanner(System.`in`)
+
+        while(!sc.hasNext()) {
+            val query = sc.next()
+                if(query in map)
+                    println("$query=${map[query]}")
+                else
+                    println("Not found")
+        }
+    }
+}
+
+fun fillMap08(n: Int){
+    for (i in 1..n) {
+        val key = read()
+        val value = read()
+        if(!key.isNullOrEmpty() && value.length == 8) {
+            map[key] = value
+        }
+    }
+}

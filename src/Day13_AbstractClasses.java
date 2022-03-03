ff700be71fce72548bd89242bd5a8b6e1bea1345
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2022-03-02 20:16:12 -0400

    Solved day 13

---
+++ src/Day13_AbstractClasses.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Day13_AbstractClasses {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String tit = sc.nextLine();
+        String aut = sc.nextLine();
+        int pri = sc.nextInt();
+        MyBook myBook = new MyBook(tit, aut, pri);
+        myBook.display();
+    }
+}
+
+abstract class Book {
+    String title;
+    String author;
+
+    public Book(String title, String author) {
+         this.title = title;
+         this.author = author;
+    }
+
+    abstract void display();
+}
+
+class MyBook extends Book{
+    int price;
+
+    public MyBook(String title, String author, int price) {
+        super(title, author);
+        this.price = price;
+    }
+
+    @Override
+    void display() {
+        System.out.println("Title: "+ title +
+                "\nAuthor: "+ author +
+                "\nPrice: "+ price);
+    }
+}
+

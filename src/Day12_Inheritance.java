3080da3d375070a621595c3d9df58a4a51254188
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2022-03-02 20:15:56 -0400

    Modify inheritance problem

--- src/Day12_Inheritance.java
+++ src/Day12_Inheritance.java
@@ -13,39 +13,48 @@ public class Day12_Inheritance {
             for (int i = 0; i < n; i++) {
                 lis[i] = sc.nextInt();
             }
-            Student st = new Student(fiName, laName, idNum, lis);
-            System.out.println("Name: " + st.lastName + ", " + st.firstName +
-                    "\nID: "+ st.idNumber+
-                    "\nGrade: " + st.grade);
+            Student stud = new Student(fiName, laName, idNum, lis);
+            stud.displayPerson();
+            stud.displayGrade();
         }
     }
 }
 
 class Student extends Person2 {
-    int idNumber;
     int[] score;
-    char grade;
 
-    public Student(String fn, String ln, int id, int[] score) {
-        super(fn, ln);
-        this.idNumber = id;
+    public Student(String firstName, String lastName, int idNumber, int[] score) {
+        super(firstName, lastName, idNumber);
         this.score = score;
+    }
+
+    public char calculate() {
         int average = (int) Arrays.stream(score).average().getAsDouble();
-        if(average >= 90 && average <= 100) grade = 'O';
-        else if(average >= 80 && average < 90) grade = 'E';
-            else if(average >= 70 && average < 80) grade = 'A';
-                else if(average >= 55 && average < 70) grade = 'P';
-                    else if(average >= 40 && average < 55) grade = 'D';
-                        else grade = 'T';
+        if(average >= 90 && average <= 100) return 'O';
+        else if(average >= 80 && average < 90) return 'E';
+        else if(average >= 70 && average < 80) return 'A';
+        else if(average >= 55 && average < 70) return 'P';
+        else if(average >= 40 && average < 55) return 'D';
+        else return 'T';
+    }
+
+    public void displayGrade() {
+        System.out.println("Grade: " + calculate());
     }
 }
 
 class Person2 {
     String firstName;
     String lastName;
+    int idNumber;
 
-    public Person2(String fn, String ln) {
-        this.firstName = fn;
-        this.lastName = ln;
+    public Person2(String firstName, String lastName, int idNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+    }
+
+    public void displayPerson() {
+        System.out.println("Name: " + lastName + ", " + firstName+ "\nID: "+ idNumber);
     }
 }

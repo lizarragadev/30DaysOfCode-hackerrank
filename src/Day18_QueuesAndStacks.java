e4656ac8d0320f0f05fdaf51da07997fa9fe68ac
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2022-03-08 22:43:36 -0400

    Changes in day 18

--- src/Day18_QueuesAndStacks.java
+++ src/Day18_QueuesAndStacks.java
@@ -27,19 +27,15 @@ public class Day18_QueuesAndStacks {
         String input = scan.nextLine();
         scan.close();
 
-        // Convert input String to an array of characters:
         char[] s = input.toCharArray();
 
-        // Create a Solution object:
         Day18_QueuesAndStacks p = new Day18_QueuesAndStacks();
 
-        // Enqueue/Push all chars to their respective data structures:
         for (char c : s) {
             p.pushCharacter(c);
             p.enqueueCharacter(c);
         }
 
-        // Pop/Dequeue the chars at the head of both data structures and compare them:
         boolean isPalindrome = true;
         for (int i = 0; i < s.length/2; i++) {
             if (p.popCharacter() != p.dequeueCharacter()) {
@@ -48,7 +44,6 @@ public class Day18_QueuesAndStacks {
             }
         }
 
-        //Finally, print whether string s is palindrome or not.
         System.out.println( "The word, " + input + ", is "
                 + ( (!isPalindrome) ? "not a palindrome." : "a palindrome." ) );
     }

78714b42fec024337a8a667c957bae15b0a6fb41
Gustavo Lizárraga <lizarraga.dev@gmail.com>
2022-03-17 23:49:49 -0400

    Partially solved day 28

---
+++ src/Day28_RegExPatternsAndIntroToDatabases.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    val N = readLine()!!.trim().toInt()
+
+    for (NItr in 1..N) {
+        val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+        val firstName = first_multiple_input[0]
+
+        val emailID = first_multiple_input[1]
+    }
+}
